5023f9baa6ae80e94721edeadedefc50e55b4003
MastroCoder <fe03mastromauro@gmail.com>
2024-03-30 17:16:21 -0300

    Inicio graficos nivo

---
+++ src/app/ui/dashboard/graphs.tsx
@@ -0,0 +1,182 @@
+import { ResponsiveCalendar } from '@nivo/calendar'
+import { ResponsiveStream } from '@nivo/stream'
+import { ResponsiveBump } from '@nivo/bump'
+
+export const DashboardCalendarGraph = ({ data }: {data: any}) => (
+    <ResponsiveCalendar
+        data={data}
+        from="2016-04-12"
+        to="2016-31-12"
+        emptyColor="#eeeeee"
+        colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'row',
+                translateY: 36,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 36,
+                itemsSpacing: 14,
+                itemDirection: 'right-to-left'
+            }
+        ]}
+    />
+)
+
+export const DashboardStreamGraph = ({ data }: {data: any}) => (
+    <ResponsiveStream
+        data={data}
+        keys={[
+            'Raoul',
+            'Josiane',
+            'Marcel',
+            'René',
+            'Paul',
+            'Jacques'
+        ]}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: '',
+            legendOffset: 36,
+            truncateTickAt: 0
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: '',
+            legendOffset: -40,
+            truncateTickAt: 0
+        }}
+        enableGridX={true}
+        enableGridY={false}
+        offsetType="silhouette"
+        colors={{ scheme: 'nivo' }}
+        fillOpacity={0.85}
+        borderColor={{ theme: 'background' }}
+        defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: '#2c998f',
+                size: 4,
+                padding: 2,
+                stagger: true
+            },
+            {
+                id: 'squares',
+                type: 'patternSquares',
+                background: 'inherit',
+                color: '#e4c912',
+                size: 6,
+                padding: 2,
+                stagger: true
+            }
+        ]}
+        fill={[
+            {
+                match: {
+                    id: 'Paul'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'Marcel'
+                },
+                id: 'squares'
+            }
+        ]}
+        dotSize={8}
+        dotColor={{ from: 'color' }}
+        dotBorderWidth={2}
+        dotBorderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    0.7
+                ]
+            ]
+        }}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'column',
+                translateX: 100,
+                itemWidth: 80,
+                itemHeight: 20,
+                itemTextColor: '#999999',
+                symbolSize: 12,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000000'
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+)
+
+export const DashboardBumpGraph = ({data}:{data:any}) => (
+    <ResponsiveBump
+        data={data}
+        colors={{ scheme: 'spectral' }}
+        lineWidth={3}
+        activeLineWidth={6}
+        inactiveLineWidth={3}
+        inactiveOpacity={0.15}
+        pointSize={10}
+        activePointSize={16}
+        inactivePointSize={0}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={3}
+        activePointBorderWidth={3}
+        pointBorderColor={{ from: 'serie.color' }}
+        axisTop={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: '',
+            legendPosition: 'middle',
+            legendOffset: -36,
+            truncateTickAt: 0
+        }}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: '',
+            legendPosition: 'middle',
+            legendOffset: 32,
+            truncateTickAt: 0
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'ranking',
+            legendPosition: 'middle',
+            legendOffset: -40,
+            truncateTickAt: 0
+        }}
+        margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+        axisRight={null}
+    />
+)
