7ed773ee38aa5d3575bb63ce00ae8ed4f899958b
MastroCoder <fe03mastromauro@gmail.com>
2024-05-25 17:48:19 -0300

    removido modo dark mode do card

--- src/app/ui/dashboard/card.tsx
+++ src/app/ui/dashboard/card.tsx
@@ -1,8 +1,7 @@
 export default function Card({ children }: { children: React.ReactNode }){
     return(
         <div className={`flex-1 flex-col h-60 max-h-full max-w-full m-3 p-5 bg-white border border-gray-200 
-        rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 
-        dark:border-gray-700 dark:hover:bg-gray-700`}>
+        rounded-lg shadow hover:bg-gray-100`}>
             {children}
         </div>
     )
